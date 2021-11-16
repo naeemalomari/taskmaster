a06e5a02b5da29b0d3130ea8a6e9177cfe74891f
naeemalomari <naeemalomari96@gmail.com>
2021-11-16 19:36:13 +0200

    finished lab38

--- app/src/main/java/com/example/taskmaster/SignUpActivity.java
+++ app/src/main/java/com/example/taskmaster/SignUpActivity.java
@@ -2,6 +2,9 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,7 +35,7 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         configureAmplify();
-
+        createNotificationChannel();
 
         Button signUp = findViewById(R.id.btnlogin);
         EditText username = findViewById(R.id.eteusername);
@@ -81,6 +84,22 @@ public class SignUpActivity extends AppCompatActivity {
                     Log.e(TAG, "signUp failed: " + error.toString());
                 });
 
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(PushListenerService.CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     private void configureAmplify() {
